d6915cded8e73218cab20706a17ccf0d5a3e258e
Roderick Liao <liaoroderickpersonal@gmail.com>
2026-01-31 13:28:54 -0500

    Basic Colour Detection
    Basic Colour Detection Test

---
+++ constants.hpp
@@ -0,0 +1,7 @@
+namespace clr{
+constexpr unsigned int S0 = 7;
+constexpr unsigned int S1 = 8;
+constexpr unsigned int S2 = 11;
+constexpr unsigned int S3 = 12;
+constexpr unsigned int S_Out = 13;
+}
