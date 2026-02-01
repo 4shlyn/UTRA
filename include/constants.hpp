c0f4c7dc39978879788bcf8485acef9b7e2a2f04
Nicholas Cheng <74994367+Miniongolf@users.noreply.github.com>
2026-01-31 22:16:24 -0500

    feat: ir test

--- include/constants.hpp
+++ include/constants.hpp
@@ -1,9 +1,4 @@
-// Drivetrain motor pins
-namespace dt {
-constexpr unsigned int EN_A = 9;
-constexpr unsigned int IN1_A = 2;
-constexpr unsigned int IN2_A = 3;
-constexpr unsigned int EN_B = 10;
-constexpr unsigned int IN1_B = 4;
-constexpr unsigned int IN2_B = 5;
+namespace ir {
+  constexpr unsigned int OUT_1 = 7;
+  constexpr unsigned int OUT_2 = 8;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,17 @@
 #include <Arduino.h>
-#include <L298NX2.h>
 #include "constants.hpp"
-#include <Servo.h>
-#include "movements.h"
-
-Servo myServo;
-
-constexpr int SERVO_PIN = 6;
-int pos = 0; // servo pos
-L298NX2 drivetrain(dt::EN_A, dt::IN1_A, dt::IN2_A, dt::EN_B, dt::IN1_B, dt::IN2_B);
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(ir::OUT_1, INPUT);
+  pinMode(ir::OUT_2, INPUT);
   Serial.begin(9600);
-  Serial.println("Testing (pls work)");
-  myServo.attach(SERVO_PIN);
-  while (!Serial)
-  {
-    //do nothing
-  }
-
-  drivetrain.setSpeedA(255);
-  drivetrain.setSpeedB(255);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  drivetrain.runA(L298N::FORWARD);
-  drivetrain.runB(L298N::FORWARD);
-  
-  
-  elevateBox();
-  delay(1000); 
-  lowerBox();
-  delay(1000); 
+  delay(250);
+  int val1 = digitalRead(ir::OUT_1);
+  int val2 = digitalRead(ir::OUT_2);
+  Serial.println("IR1: " + String(val1) + " IR2: " + String(val2));
 }
